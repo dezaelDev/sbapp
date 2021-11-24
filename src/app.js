16aa74818e82316947b9dcf2f7caa97d2ba7bd42
dezaelDev <emmanuelfernandez.developer@gmail.com>
2021-11-24 15:25:27 -0300

    se agrega y configura swagger, cors y morgan

--- src/app.js
+++ src/app.js
@@ -1,7 +1,20 @@
 import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUI from 'swagger-ui-express';
+import { options } from './swagger-options';
+
 import bookingsRoutes from './routes/bookings.routes';
 
 const app = express();
+const specs = swaggerJSDoc(options);
+
+app.use(cors());
+app.use(morgan('dev'));
 app.use(express.json());
+app.use('/docs', swaggerUI.serve, swaggerUI.setup(specs));
 app.use(bookingsRoutes);
+
 export default app;
