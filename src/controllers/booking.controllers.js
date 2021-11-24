97313b1e8f5483d248a4a2cafd372b82bc9b7608
dezaelDev <emmanuelfernandez.developer@gmail.com>
2021-11-24 15:28:22 -0300

    se agregan controles de error mediante trycatch

--- src/controllers/booking.controllers.js
+++ src/controllers/booking.controllers.js
@@ -2,28 +2,44 @@ import { connect } from '../database';
 
 //Obtener todas las reservas
 export const getBookings = async (req, res) => {
-	const connection = await connect();
-
-	const [row] = await connection.query(`SELECT * FROM ReservedWork`);
-	res.json(row);
+	try {
+		const connection = await connect();
+		const [row] = await connection.query(`SELECT * FROM ReservedWork`);
+		res.json(row);
+	} catch (error) {
+		console.error(error);
+		res.sendStatus(400);
+	}
 };
 
 //obtener una reserva por id
 export const getBooking = async (req, res) => {
-	const connection = await connect();
+	try {
+		const connection = await connect();
 
-	const [row] = await connection.query(
-		`SELECT * FROM ReservedWork WHERE id=?`,
-		[req.params.id]
-	);
-	res.json(row[0]);
+		const [row] = await connection.query(
+			`SELECT * FROM ReservedWork WHERE id=?`,
+			[req.params.id]
+		);
+		res.json(row[0]);
+	} catch (error) {
+		console.error(error);
+		res.sendStatus(400);
+	}
 };
 
 //obtener la cantidad de reservas
 export const getBookingsCount = async (req, res) => {
-	const connection = await connect();
-	const [row] = await connection.query(`SELECT COUNT(*) FROM ReservedWork`);
-	res.json(row[0]['COUNT(*)']);
+	try {
+		const connection = await connect();
+		const [row] = await connection.query(
+			`SELECT COUNT(*) FROM ReservedWork`
+		);
+		res.json(row[0]['COUNT(*)']);
+	} catch (error) {
+		console.error(error);
+		res.sendStatus(400);
+	}
 };
 
 //guardar una reserva POST request
@@ -33,20 +49,30 @@ export const saveBooking = async (req, res) => {
 
 //borrar una reserva DELETE request
 export const deleteBooking = async (req, res) => {
-	const connection = await connect();
-	const result = await connection.query(
-		'DELETE FROM ReservedWork WHERE id=?',
-		[req.params.id]
-	);
-	res.sendStatus(204);
+	try {
+		const connection = await connect();
+		const result = await connection.query(
+			'DELETE FROM ReservedWork WHERE id=?',
+			[req.params.id]
+		);
+		res.sendStatus(204);
+	} catch (error) {
+		console.error(error);
+		res.sendStatus(400);
+	}
 };
 
 //actualiza una reserva
 export const updateBooking = async (req, res) => {
-	const connection = await connect();
-	const result = await connection.query(
-		'UPDATE ReservedWork SET ? WHERE id=?',
-		[req.body, req.params.id]
-	);
-	console.log(result);
+	try {
+		const connection = await connect();
+		const result = await connection.query(
+			'UPDATE ReservedWork SET ? WHERE id=?',
+			[req.body, req.params.id]
+		);
+		res.sendStatus(204);
+	} catch (error) {
+		console.error(error);
+		res.sendStatus(400);
+	}
 };
