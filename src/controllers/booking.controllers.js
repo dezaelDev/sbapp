51cc504bc96532dd44a37f8d379c788a8d96c16f
dezaelDev <emmanuelfernandez.developer@gmail.com>
2021-11-24 00:03:19 -0300

    se agregan consultas de las funciones definidas del crud

--- src/controllers/booking.controllers.js
+++ src/controllers/booking.controllers.js
@@ -1,29 +1,52 @@
+import { connect } from '../database';
+
 //Obtener todas las reservas
-export const getBookings = (req, res) => {
-	res.send('Hello world');
+export const getBookings = async (req, res) => {
+	const connection = await connect();
+
+	const [row] = await connection.query(`SELECT * FROM ReservedWork`);
+	res.json(row);
 };
 
 //obtener una reserva por id
-export const getBooking = (req, res) => {
-	res.send('Hello world');
+export const getBooking = async (req, res) => {
+	const connection = await connect();
+
+	const [row] = await connection.query(
+		`SELECT * FROM ReservedWork WHERE id=?`,
+		[req.params.id]
+	);
+	res.json(row[0]);
 };
 
 //obtener la cantidad de reservas
-export const getBookingsCount = (req, res) => {
+export const getBookingsCount = async (req, res) => {
+	const connection = await connect();
+	const [row] = await connection.query(`SELECT COUNT(*) FROM ReservedWork`);
+	res.json(row[0]['COUNT(*)']);
+};
+
+//guardar una reserva POST request
+export const saveBooking = async (req, res) => {
 	res.send('Hello world');
 };
 
-//guardar una reserva
-export const saveBooking = (req, res) => {
-	res.send('Hello world');
-};
-
-//borrar una reserva
-export const deleteBooking = (req, res) => {
-	res.send('Hello world');
+//borrar una reserva DELETE request
+export const deleteBooking = async (req, res) => {
+	const connection = await connect();
+	const result = await connection.query(
+		'DELETE FROM ReservedWork WHERE id=?',
+		[req.params.id]
+	);
+	res.sendStatus(204);
 };
 
 //actualiza una reserva
-export const updateBooking = (req, res) => {
-	res.send('Hello world');
+export const updateBooking = async (req, res) => {
+	const connection = await connect();
+	const result = await connection.query(
+		'UPDATE ReservedWork SET ? WHERE id=?',
+		[req.body, req.params.id]
+	);
+	console.log(result);
 };
